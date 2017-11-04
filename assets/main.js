0140ec96a6908d766ac17ac12ac6598519f8b013
Isaac <isaac@wilcove.com>
2017-11-04 12:26:08 -0700

    Created server-client communication feedback system

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,11 @@
 const socket = new WebSocket('ws://localhost:8003');
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('ShalevBito-2753');
-});
-
 // Listen for messages
 socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
+    console.log('Received message from server:', event.data);
+    var playerdata = JSON.parse(event.data);
 });
+
+function sendBattletag() {
+    socket.send(document.getElementById("btag").value);
+}
