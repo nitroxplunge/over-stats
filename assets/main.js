177d629ca02898e8d27ab8439ea73a5561f73eae
Isaac <isaac@wilcove.com>
2017-11-04 14:00:00 -0700

    Added more stats

--- assets/main.js
+++ assets/main.js
@@ -4,12 +4,15 @@ const socket = new WebSocket('ws://localhost:8003');
 socket.addEventListener('message', function (event) {
     console.log('Received message from server:', event.data);
     var playerdata = JSON.parse(event.data);
-    var level = playerdata.profile.level;
-    document.getElementById("lvl").innerHTML = "Level: " + level;
-    console.log(level);
+    document.getElementById("lvl").innerHTML = "Level: " + playerdata.profile.level;
+    document.getElementById("sr").innerHTML = "SR: " + playerdata.profile.rank;
+    document.getElementById("kda").innerHTML = "KDA: " + playerdata.competitive.global.eliminations / playerdata.competitive.global.deaths;
+    document.getElementById("rankpic").src = playerdata.profile.rankPicture;
+    document.getElementById("fetching").innerHTML = "";
 });
 
 function sendBattletag() {
+    document.getElementById("fetching").innerHTML = "Fetching Stats...";
     var BTag = document.getElementById("btag").value;
     BTag = BTag.replace("#", "-");
     socket.send(BTag);
